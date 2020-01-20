b19ccbd51ae01cbb50d2d70f9219c6ff180f1b08
THAT BOY <zhu.thatboy@outlook.com>
2020-01-20 17:21:16 +0800

    由于本机opencv库的整合，变更了项目的附加包含目录和附加库目录。

--- 颜色量化/ColorCube.cpp
+++ 颜色量化/ColorCube.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 
 
-// 监测范围异常
+// 测试范围异常
 #ifdef  _DEBUG
 #define ASSERT_CUBE	\
 if (bBegin > bEnd\
@@ -30,21 +30,21 @@ bool thatboy::ColorCube::doSplitCube(int channel)
 		if (bBegin == bEnd)
 			break;
 		size_t curSize = 0;
+		size_t lastSize;
 		for (size_t b = bBegin; b <= bEnd; b++)
 		{
+			lastSize = curSize;
 			for (size_t g = gBegin; g <= gEnd; g++)
 				for (size_t r = rBegin; r <= rEnd; r++)
 					curSize += colorHist[b][g][r];
 			if (curSize >= size / 2)
 			{
+				if (lastSize > size - curSize)// 分给左侧
+					--b;
 				// 产生新的空间，计算位置和数量
 				// 修改本空间的B结束位置和数量
-				ColorCube newCube(b, gBegin, rBegin, bEnd, gEnd, rEnd);
-				bEnd = b - 1;
-				if (b == bBegin) {
-					newCube.bBegin = b + 1;
-					bEnd = b;
-				}
+				ColorCube newCube(b + 1, gBegin, rBegin, bEnd, gEnd, rEnd);
+				bEnd = b;
 				newCube.reshapeCube();
 				colorCubeSet.push_back(newCube);
 				reshapeCube();
@@ -59,21 +59,21 @@ bool thatboy::ColorCube::doSplitCube(int channel)
 		if (gBegin == gEnd)
 			break;
 		size_t curSize = 0;
+		size_t lastSize;
 		for (size_t g = gBegin; g <= gEnd; g++)
 		{
+			lastSize = curSize;
 			for (size_t b = bBegin; b <= bEnd; b++)
 				for (size_t r = rBegin; r <= rEnd; r++)
 					curSize += colorHist[b][g][r];
 			if (curSize >= size / 2)
 			{
+				if (lastSize > size - curSize)// 分给左侧
+					--g;
 				// 产生新的空间，计算位置和数量
 				// 修改本空间的B结束位置和数量
-				ColorCube newCube(bBegin, g, rBegin, bEnd, gEnd, rEnd);
-				gEnd = g - 1;
-				if (g == gBegin) {
-					newCube.gBegin = g + 1;
-					gEnd = g;
-				}
+				ColorCube newCube(bBegin, g + 1, rBegin, bEnd, gEnd, rEnd);
+				gEnd = g;
 				newCube.reshapeCube();
 				colorCubeSet.push_back(newCube);
 				reshapeCube();
@@ -88,21 +88,21 @@ bool thatboy::ColorCube::doSplitCube(int channel)
 		if (rBegin == rEnd)
 			break;
 		size_t curSize = 0;
+		size_t lastSize;
 		for (size_t r = rBegin; r <= rEnd; r++)
 		{
+			lastSize = curSize;
 			for (size_t b = bBegin; b <= bEnd; b++)
 				for (size_t g = gBegin; g <= gEnd; g++)
 					curSize += colorHist[b][g][r];
 			if (curSize >= size / 2)
 			{
+				if (lastSize > size - curSize)// 分给左侧
+					--r;
 				// 产生新的空间，计算位置和数量
 				// 修改本空间的B结束位置和数量
-				ColorCube newCube(bBegin, gBegin, r, bEnd, gEnd, rEnd);
-				rEnd = r - 1;
-				if (r == rBegin) {
-					newCube.rBegin = r + 1;
-					rEnd = r;
-				}
+				ColorCube newCube(bBegin, gBegin, r + 1, bEnd, gEnd, rEnd);
+				rEnd = r;
 				newCube.reshapeCube();
 				colorCubeSet.push_back(newCube);
 				reshapeCube();
@@ -171,7 +171,7 @@ bool thatboy::ColorCube::splitBrotherCube()
 	auto channel = length[0] > length[1] ? length[0] > length[2] ? 0 : 2 : length[1] > length[2] ? 1 : 2;
 
 	return doSplitCube(channel);
-	// 分裂失败则尝试其他维度
+
 	//if (calcSplitCube(channel))
 	//	return true;
 	////length[channel] = 0;
@@ -223,7 +223,7 @@ void thatboy::ColorCube::splitCube(size_t cubeNumber)
 				++iter;
 		if (iter == colorCubeSet.rend())	// 找不到可以分裂的空间
 			break;
-       // std::max_element(colorCubeSet.begin(), colorCubeSet.end())->splitBrotherCube();// 速度较慢
+       // std::max_element(colorCubeSet.begin(), colorCubeSet.end())->splitBrotherCube();// 
     }
 	colorCubeSet.sort();
 
--- 颜色量化/Main.cpp
+++ 颜色量化/Main.cpp
@@ -7,14 +7,24 @@ int main()
 	using namespace thatboy;
 
 	Mat srcImage, dstImage;
-	srcImage = imread("baboon.jpg");	/* "baboon.jpg" */
+	srcImage = imread("color3.png");	/* "baboon.jpg" */
 	if (srcImage.empty())
 		throw std::bad_exception();
 	dstImage = srcImage.clone();
 
-	ColorCube::loadImage(srcImage);
-	ColorCube::splitCube();
-	ColorCube::write8bIndexBmp("bliss.bmp");
+	// 测试二十次运算
+	auto begin = clock();
+	for (size_t i = 0; i < 20; i++)
+	{
+		ColorCube::loadImage(srcImage);
+		ColorCube::splitCube();
+	}
+	auto end = clock();
+
+	cout << (end - begin) / 20000.0 << "s/张" << endl
+		<< (end - begin)/1000.0 << "s/20张" << endl;
+
+	ColorCube::write8bIndexBmp("baboon.bmp");
 
 	for (auto iter = dstImage.begin<Vec3b>(); iter != dstImage.end<Vec3b>(); ++iter)
 		*iter = ColorCube::mapping(*iter);
